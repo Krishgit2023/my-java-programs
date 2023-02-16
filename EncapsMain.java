f3b2c19b60fce56400f092d52cd2ba8111d9d627
Krishgit2023 <124280727+Krishgit2023@users.noreply.github.com>
2023-02-16 12:10:31 +0530

    Encapsulation

---
+++ EncapsMain.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class EncapsMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Encaps obj = new Encaps();
+		obj.setId(10);
+		System.out.println(obj.getId());
+		obj.setName("Krishna");
+		System.out.println(obj.getName());
+	}
+
+}
